62a5efdcf76da670ca551a05bda792b23f0527b0
이하늘 <wsxedc901@gmail.com>
2022-10-13 11:12:22 +0900

    사라진 back 관련 코드들 복구

---
+++ back/src/users/users.validation.js
@@ -0,0 +1,16 @@
+import { Joi } from "express-validation";
+
+export default {
+  body: Joi.object({
+    userId: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    nickname: Joi.string().required(),
+    role: Joi.string(),
+    name: Joi.string().required(),
+    phoneNumber: Joi.string(),
+    profileImage: Joi.string(),
+    userDesc: Joi.string(),
+    createdAt: Joi.string(),
+  }),
+};
