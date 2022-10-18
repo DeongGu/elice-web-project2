3d4da0cfc2713c065fec7b5500eadd3aaa9dd76c
[AI5/1반] 이태의 <wishty@gmail.com>
2022-10-18 14:32:59 +0900

    불용 컨포넌트 정리

--- back/src/routes.js
+++ back/src/routes.js
@@ -1,10 +1,6 @@
 import usersRoutes from "./componets/users/users.routes";
 import itemRoutes from "./componets/items/items.routes";
 // import dibsRoutes from "./componets/dibs/dibs.routes";
-// import orderRoutes from "./componets/orders/orders.routes";
-// import deliveryRoutes from "./componets/deliveries/deliveries.routes";
-// import reviewRoutes  from "./componets/reviews/reviews.routes";
-// import adderssRoutes  from "./componets/adderss/adderss.routes";
 
 export default (App) => {
   const router = App;
@@ -17,8 +13,4 @@ export default (App) => {
   router.use("/", usersRoutes);
   router.use("/", itemRoutes);
   // router.use("/", dibsRoutes);
-  // router.use("/", orderRoutes);
-  // router.use("/", deliveryRoutes);
-  // router.use("/", reviewRoutes);
-  // router.use("/", adderssRoutes);
 };
