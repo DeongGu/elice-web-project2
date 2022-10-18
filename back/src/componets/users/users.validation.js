bc9d89fa8d37a5c8018e211ddbe682dc807e21e3
HHJ <ljehhj2632@gmail.com>
2022-10-18 14:15:06 +0900

    componets/users/users.validation.js 추가

---
+++ back/src/componets/users/users.validation.js
@@ -0,0 +1,23 @@
+import { Joi } from "joi";
+import { clientSideError } from "../middlewares/errorHandler";
+
+const userSchema = Joi.object({
+  userId: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  nickname: Joi.string().required(),
+  role: Joi.string(),
+  name: Joi.string().required(),
+  phoneNumber: Joi.string(),
+  profileImage: Joi.string(),
+  userDesc: Joi.string(),
+});
+
+function userValidation(req, res, next) {
+  const body = req.body;
+  const { error, value } = userSchema.validate(body);
+  if (error) throw new clientSideError("User information is not valid");
+  next();
+}
+
+export default userValidation;
