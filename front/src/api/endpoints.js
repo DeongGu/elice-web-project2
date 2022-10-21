7d60bbb2252428786dc056ee7fa685cc76b59e95
이하늘 <wsxedc901@gmail.com>
2022-10-22 02:05:28 +0900

    . 추가 된 거 롤백

--- front/src/api/endpoints.js
+++ front/src/api/endpoints.js
@@ -4,9 +4,7 @@ const CHECK_USER = ['get', 'users'];
 const CHECK_OTHER_USER = ['get', 'users'];
 const EDIT_USER = ['put', 'users'];
 const DELETE_USER = ['delete', 'users'];
-
 const CHECK_ITEMS = ['get', 'items'];
-
 const CHECK_DIBS = ['get', 'dibs'];
 
 export {
